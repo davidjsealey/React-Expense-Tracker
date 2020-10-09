353e497b9398a3b60782daf7871ac27d363b43b3
David J Sealey <david.sealey@soak.co.uk>
2020-10-09 14:46:14 +0100

    Feature: Remove IDs

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -4,12 +4,12 @@ export const IncomeExpenses = () => {
     return (
         <div className="inc-exp-container">
             <div>
-            <h4>Income</h4>
-            <p id="money-plus" className="money plus">+$0.00</p>
-            </div>
-            <div>
-            <h4>Expense</h4>
-            <p id="money-minus" className="money minus">-$0.00</p>
+                <h4>Income</h4>
+                <p className="money plus">+$0.00</p>
+                </div>
+                <div>
+                <h4>Expense</h4>
+                <p className="money minus">-$0.00</p>
             </div>
       </div>
     )
